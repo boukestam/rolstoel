ae19bcdb31a609e4fbf927e33dec06fe2a83eeda
ScottMackay2 <scottmackay@live.nl>
2015-03-31 12:37:20 +0200

    Documentated the following files: - SensorListener - MyRangeSensor - Task

--- src/sensors/SensorListener.java
+++ src/sensors/SensorListener.java
@@ -1,10 +1,17 @@
-/*
- * Auteur: Timon van den Brink
- * Datum: 3-10-2015
- */
-
 package sensors;
 
+/**
+ * This interface specifies for everything that uses this will be a listener for a sensor.
+ * @author Timon van den Brink
+ * @version 1.0
+ */
+
 public interface SensorListener {
+	
+	/**
+	 * Method that must be implemented by all listeners of this interface. It gives the new value of the listener and the sensor who gave the message.
+	 * @param source the sensor which the new value came from.
+	 * @param value the new given value by the sensor.
+	 */
 	public void valueChanged(Sensor source, int value);
 }
--- src/sensors/Task.java
+++ src/sensors/Task.java
@@ -1,21 +1,42 @@
-/*
- * Auteur: Bouke Stam
- * Datum: 3-10-2015
- */
-
 package sensors;
+
+/**
+ * This class combines variables in one object that handles the interval between updates of an updatable object.
+ * @author Bouke Stam
+ * @version 1.0
+ */
 
 public class Task {
 
+	/**
+	 * Time between updates.
+	 */
 	private int interval;
+	
+	/**
+	 * Last time the interval has passed.
+	 */
 	private long lastExecuted;
+	
+	/**
+	 * the object that must be updated.
+	 */
 	private Updatable updatable;
 	
+	/**
+	 * The constructor saving the updatable object and interval between updates.
+	 * @param updatable the updatable object that gets saved as global variable.
+	 * @param interval the interval between a next possible update.
+	 */
 	public Task(Updatable updatable,int interval){
 		this.updatable=updatable;
 		this.interval=interval;
 	}
 	
+	/**
+	 * This method gets called to trigger an update. It also checks if the interval has been passed. If the interval is not passed this method does nothing.
+	 * @param time the current time.
+	 */
 	public void update(long time){
 		if(lastExecuted+interval<=time){
 			execute();
@@ -23,6 +44,9 @@ public class Task {
 		}
 	}
 	
+	/**
+	 * Triggers thhe update() method of an updatable object.
+	 */
 	public void execute(){
 		updatable.update();
 	}
