9300709aa822800287f6000896c9934f4f17d9c5
NoahSte <noah97ste@gmail.com>
2015-03-31 13:14:42 +0200

    + Added documentation. - None * Changed: Controller.java

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,24 +1,41 @@
-/*
- * Auteur: Scott Mackay
- * Datum: 3-10-2015
- */
 
 package controllers;
 
 import sensors.Sensor;
 import sensors.SensorListener;
 
+/**
+ * This controller lets other controllers switch between each other, 
+ * it can turn controllers on or off 
+ * and it calls the control method of the currently running controllers.
+ * @author Scott Mackay
+ * @version 1.0
+ */
+
 public abstract class Controller extends Thread implements SensorListener{
 	
+	/**
+	 * the current caller object.
+	 */
 	private Controller caller;
+	/**
+	 * determines if the controller's controller method is called.
+	 */
 	private boolean isRunning;
-
+	
+	/**
+	 * Pauses the caller and turns on the controller.
+	 * @param caller lets the provided caller pause.
+	 */
 	public void switchToController(Controller caller){
 		caller.stopController();
 		this.caller=caller;
 		this.startController();
 	}
 	
+	/**
+	 * Starts the thread for controller.
+	 */
 	public void startController(){
 		this.onStart();
 		if(!this.isAlive()){
@@ -27,25 +44,42 @@ public abstract class Controller extends Thread implements SensorListener{
 		this.setRunning(true);
 	}
 	
+	/**
+	 * Stops the thread for controller.
+	 */
 	public void stopController(){
 		this.onStop();
 		this.setRunning(false);
 	}
 	
+	/**
+	 * Pauses the controller and switches back to the current caller.
+	 */
 	public void switchBackToCaller(){
 		this.stopController();
 		caller.startController();
 		caller=null;
 	}
 	
+	/**
+	 * Switches the isRunning boolean to the provided argument.
+	 * @param state the boolean that isRunning gets set to.
+	 */
 	public void setRunning(boolean state){
 		this.isRunning=state;
 	}
 	
+	/**
+	 * Returns the isRunning variable, which is true if the thread is supposed to run and false otherwise.
+	 * @return the isRunning boolean.
+	 */
 	public boolean isRunning(){
 		return this.isRunning;
 	}
 	
+	/**
+	 * Calls the control method of the current caller if isRunning is true.
+	 */
 	public void run(){
 		while(true){
 			if(this.isRunning()){
@@ -58,15 +92,24 @@ public abstract class Controller extends Thread implements SensorListener{
 		}
 	}
 	
-	//This is where sleeping should be done if needed
+	/**
+	 * This is where sleeping should be done if needed
+	 */
 	public abstract void control();
 	
+	/**
+	 * This is where the given sensor (source) gives it new value.
+	 */
 	public abstract void valueChanged(Sensor source,int value);
 	
-	//Gets called when controller is (re)started
+	/**
+	 * Gets called when controller is (re)started.
+	 */
 	public abstract void onStart();
 	
-	//Gets called when controller is stopped
+	/**
+	 * Gets called when controller is stopped
+	 */
 	public abstract void onStop();
 
 }
