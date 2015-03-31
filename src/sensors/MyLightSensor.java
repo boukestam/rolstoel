a51735288cafd218f959fad310a7be8996c7d99a
NoahSte <noah97ste@gmail.com>
2015-03-31 12:36:17 +0200

    + Added documentation to some classes. - None * Changed the classes: MyColorSensor.java, MyLightSensor.java & Updatable.java

--- src/sensors/MyLightSensor.java
+++ src/sensors/MyLightSensor.java
@@ -1,24 +1,34 @@
-/*
- * Auteur: Timon van den Brink
- * Datum: 3-10-2015
- */
-
 package sensors;
 
 import lejos.nxt.LCD;
 import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 
+/**
+ * This class lets the robot communicate with the light sensor.
+ * @author Timon van den Brink
+ * @version 1.0
+ */
+
 public class MyLightSensor extends Sensor{
 	
+	/**
+	 * The light sensor as an object.
+	 */
 	private LightSensor lightSensor;
 	
+	/**
+	 * The constructor of the light sensor which defines the hardware port of the sensor.
+	 * @param port
+	 */
 	public MyLightSensor(SensorPort port){
 		super();
 		lightSensor = new LightSensor(port);
 	}
 	
-	//1 is black, 0 is white
+	/**
+	 * Returns the value 1 if the sensor detects black and returns 0 if the sensor detects white.
+	 */
 	@Override
 	public int getValue(){
 		int percentage=lightSensor.getLightValue();
@@ -27,11 +37,17 @@ public class MyLightSensor extends Sensor{
 		return percentage>50?0:1;
 	}
 	
+	/**
+	 * Calibrates the measured value as a high value.
+	 */
 	@Override
 	public void calibrateHigh(){
 		lightSensor.calibrateHigh();
 	}
 	
+	/**
+	 * Calibrates the measured value as a low value.
+	 */
 	@Override
 	public void calibrateLow(){
 		lightSensor.calibrateLow();
